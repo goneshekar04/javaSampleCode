025619daef75747461b01ae700ee23413d53f926
goneshekar04 <goneshekar04@gmail.com>
2019-09-15 12:19:54 -0700

    test

---
+++ javaSampleCode/String/CountVowels.java
@@ -0,0 +1,23 @@
+package String;
+
+public class CountVowels {
+
+	public static void main(String[] args) {
+		String name="rajashekar";
+		char[] ch=name.toCharArray();
+		int count=0;
+		for(char c:ch) {
+		   switch(c) {
+		   case 'a':
+		   case 'e':
+		   case 'i':
+		   case 'o':
+		   case 'u':
+			   count++;
+			break;   
+		   }
+		}
+		System.out.println(count);
+	}
+
+}
